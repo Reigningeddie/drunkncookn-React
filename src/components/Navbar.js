ae87371395600a15c625f4ecb7a4e0c7888041c7
Eduardo Aguilar <eaguilar92105@yahoo.com>
2021-11-10 18:27:34 -0800

    finished js for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,40 @@
-import React from 'react'
-import './navbar.css'
+import React, { useState, useEffect } from 'react';
+import './navbar.css';
 
 
 export default function Navbar() {
+	const [toggleMenu, setToggleMenu] = useState(false)
+	const [screenWidth, setScreenWidth] = useState (window.innerWidth)
+
+	const toggleNav = () => {
+		setToggleMenu(!toggleMenu)
+	}
+
+	useEffect(() => {
+
+		const changeWidth = () => {
+			setScreenWidth(window.innerWidth);
+		}
+
+		window.addEventListener('resize', changeWidth)
+
+		return () => {
+			window.removeEventListener('resize', changeWidth)
+		}
+		
+	}, [])
+
 	return (
 		<nav>
+			{(toggleMenu || screenWidth > 500) && (
 			<ul className="list">
 				<li className="items">Home</li>
 				<li className="items">Summary</li>
 				<li className="items">Contact</li>
 			</ul>
-			<button className="btn">BTN</button>
+			)}
+
+			<button onClick={toggleNav} className="btn">BTN</button>
 		</nav>
 	)
 }
