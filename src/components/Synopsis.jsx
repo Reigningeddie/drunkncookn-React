50720509a2b07c29a211f95a0b0f7afa81b9d8ef
Eduardo Aguilar <eaguilar92105@yahoo.com>
2021-11-18 15:47:52 -0800

    added an actual synopsis to component

--- src/components/Synopsis.jsx
+++ src/components/Synopsis.jsx
@@ -5,11 +5,12 @@ export default function Synopsis() {
     return (
         <div className="wrapper">
             <div className="scroll-text">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio dolorum nostrum nulla incidunt temporibus. Iusto dolor earum aperiam voluptates sed aut vel ad dolorum at nulla, nihil placeat sit repellendus perspiciatis eveniet iure aliquam quo amet culpa accusantium nam perferendis.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio dolorum nostrum nulla incidunt temporibus. Iusto dolor earum aperiam voluptates sed aut vel ad dolorum at nulla, nihil placeat sit repellendus perspiciatis eveniet iure aliquam quo amet culpa accusantium nam perferendis.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio dolorum nostrum nulla incidunt temporibus. Iusto dolor earum aperiam voluptates sed aut vel ad dolorum at nulla, nihil placeat sit repellendus perspiciatis eveniet iure aliquam quo amet culpa accusantium nam perferendis.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio dolorum nostrum nulla incidunt temporibus. Iusto dolor earum aperiam voluptates sed aut vel ad dolorum at nulla, nihil placeat sit repellendus perspiciatis eveniet iure aliquam quo amet culpa accusantium nam perferendis.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio dolorum nostrum nulla incidunt temporibus. Iusto dolor earum aperiam voluptates sed aut vel ad dolorum at nulla, nihil placeat sit repellendus perspiciatis eveniet iure aliquam quo amet culpa accusantium nam perferendis.</p>
+                <p>After a very unfortunate situation with a cup of noodles a fork and microwave. Our protagonist, a young Howard, ends up in a coma which in turn may be a blessing in disguise.</p>
+                <p>After the few weeks of hospital stay our young protagonist needs to acclimate himself back into society. But in case your weren't familiar with who Howard was before the accident that may be easier said than done.</p>
+                <p>Luckily, for Howard he was always able to rely on his single father, Hank who immediately gets him a job. Howard however seems to find himself falling back into his old drinking habits and blackouts maybe nothing had changed, no lesson learned.</p>
+                <p>For Howard maybe, his friends however, soon discover that Howard had picked up a knack to be a very exquisite cook throughout his blackouts. Blackouts never just bring cheer and good food, lets be real, they also get Howard put on the italian mobs radar.</p>
+                <p>How and why is the mob after some no name, drunk, black'd out, dare I say?</p>
+                <p>5 star chef?</p>
             </div>
         </div>
     )
