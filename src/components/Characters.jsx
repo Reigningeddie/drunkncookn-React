07c843fbad0fdbf343fecc4cba133eee4881ab48
Eduardo Aguilar <eaguilar92105@yahoo.com>
2021-11-19 21:36:21 -0800

    creating html for the characters component

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import './characters.css'
 
 export default function Characters() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="characters-body">
+      <main>
+        <section>
+          <div className="grid">
+						<div>
+              <h2 className="names">Character Names go here</h2>
+              <p>character bio goes here</p>
+            </div>
+					</div>
+            <div className="grid character-grid">
+          </div>
+        </section>
+      </main>
+    </div>
+  )
 }
