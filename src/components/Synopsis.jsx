7cd1f12992f6e357333660b45ebc1a1126071f53
Eduardo Aguilar <eaguilar92105@yahoo.com>
2021-11-19 20:29:04 -0800

    created the star wars scroll effect

--- src/components/Synopsis.jsx
+++ src/components/Synopsis.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import './synopsis.css'
 
+//  **TODO: fix reload bug
+
 export default function Synopsis() {
     return (
         <container className="background">
             <div className="wrapper">
                 <div className="scroll-text">
-                    <h1>somewhere in a not so</h1>
-                    <br />
-                    <h1>parallel universe</h1>
-                    <br />
-                    <br />
+                    <h1>* somewhere in a not so</h1>
+                    <h1>parallel universe *</h1>
                     <br />
                     <br />
                     <p>After a very unfortunate situation with a cup of noodles, a fork and microwave. Our protagonist, a young Howard, ends up in a coma which in turn may be a blessing in disguise.</p>
-                    <p>After a few weeks of his hospital stay our young protagonist needs to acclimate himself back into society. But in case your weren't familiar with who Howard was before the</p>
-                    <p>accident that may be easier said than done. Luckily for Howard he was always able to rely on his single father, Hank, who immediately gets him a job. Howard however seems to</p>
-                    <p> find himself falling back into his old drinking habits and blackouts maybe nothing had changed, no lesson learned. For Howard maybe, his friends however, soon discover that</p>
-                    <p>Howard had picked up a knack to be a very exquisite cook throughout his blackouts. Blackouts never just bring cheer and good food, let’s be real, they also get Howard put on</p>
-                    <p>the Italian mobs radar. How and why is the mob after some no name, drunk, black'd out, dare I say.......</p>
+                    <p>After a few weeks of his hospital stay our young protagonist needs to acclimate himself back into society. But in case you weren't familiar with who Howard was before the accident that may be easier said than done.</p>
+                    <p>Luckily for Howard he was always able to rely on his single father, Hank, who immediately gets him a job.</p>
+                    <p>Howard however seems to  find himself falling back into his old drinking habits and blackouts. Maybe nothing had changed, no lesson learned for Howard, his friends on the other hand soon discover that Howard had picked up a knack to be a very exquisite cook throughout his blackouts.</p>
+                    <p>Blackouts never just bring cheer and good food, let’s be real, they also get Howard put on the Italian mobs radar.</p>
+                    <p>How and why is the mob after some no name, black'd out drunk dare I say.......</p>
+                    <br />
+                    <br />
                     <br />
                     <br />
                     <br />
