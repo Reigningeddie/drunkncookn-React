dc51dfd8a947846050837052f72bbadc93bda07e
Eduardo Aguilar <eaguilar92105@yahoo.com>
2022-04-20 19:32:59 -0700

    worked on characters 1st div

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -8,7 +8,14 @@ export default function Characters() {
     <container className={`${chars.reset} ${chars.hello}`}>
       <main>
         <section>
-          <h2 className={`${chars.uppercase} ${chars.textCompressed}`}>WITH XFAI, TRADING WILL <span className={chars.textPrimary}>NEVER</span> HAVE TO REMAIN CENTRALIZED</h2>
+          <div className="grid">
+            <div>
+              <h2 className={`${chars.uppercase} ${chars.textCompressed}`}>WITH XFAI, TRADING WILL <span className= {chars.textPrimary}>NEVER</span> HAVE TO REMAIN CENTRALIZED</h2>
+              <p>XFai develops tooling by graphing the DeFi space to build <strong>game changing</strong>products to benefit users</p><div className={`${chars.flexLarge}`}>
+            <a href="google.com" className={`${chars.btnPrimary}`}>WHITEPAPER </a> 
+            <a href="google.com" className={`${chars.btnAccentUppercase}`}>WATCH VIDEO</a></div>
+            </div>
+            </div>
         </section>
       </main>
     </container>
