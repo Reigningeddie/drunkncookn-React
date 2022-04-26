1622a077e216a1853ef9690fe67f58dc3df91952
Eduardo Aguilar <eaguilar92105@yahoo.com>
2022-04-26 14:34:19 -0700

    formatted characters.jsx

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -11,11 +11,12 @@ export default function Characters() {
           <div className="grid">
             <div>
               <h2 className={`${chars.uppercase} ${chars.textCompressed}`}>WITH XFAI, TRADING WILL <span className= {chars.textPrimary}>NEVER</span> HAVE TO REMAIN CENTRALIZED</h2>
-              <p>XFai develops tooling by graphing the DeFi space to build <strong>game changing</strong>products to benefit users</p><div className={`${chars.flexLarge}`}>
-            <a href="google.com" className={`${chars.btnPrimary}`}>WHITEPAPER </a> 
-            <a href="google.com" className={`${chars.btnAccentUppercase}`}>WATCH VIDEO</a></div>
+              <p>XFai develops tooling by graphing the DeFi space to build <strong>game changing</strong>products to benefit users</p>
+              <div className={`${chars.flexLarge}`}>
+                <a href="google.com" className={`${chars.btnPrimary}`}>WHITEPAPER </a> 
+                <a href="google.com" className={`${chars.btnAccentUppercase}`}>WATCH VIDEO</a></div>
             </div>
-            </div>
+          </div>
         </section>
       </main>
     </container>
