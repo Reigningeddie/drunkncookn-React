a0f6dfedc193d87debb8086cc9e913c2bae2fc7a
Eduardo Aguilar <eaguilar92105@yahoo.com>
2022-04-26 15:10:46 -0700

    fixed css modules

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -8,14 +8,17 @@ export default function Characters() {
     <container className={`${chars.reset} ${chars.hello}`}>
       <main>
         <section>
-          <div className="grid">
+          <div className={chars.grid}>
             <div>
               <h2 className={`${chars.uppercase} ${chars.textCompressed}`}>WITH XFAI, TRADING WILL <span className= {chars.textPrimary}>NEVER</span> HAVE TO REMAIN CENTRALIZED</h2>
               <p>XFai develops tooling by graphing the DeFi space to build <strong>game changing</strong>products to benefit users</p>
-              <div className={`${chars.flexLarge}`}>
-                <a href="google.com" className={`${chars.btnPrimary}`}>WHITEPAPER </a> 
-                <a href="google.com" className={`${chars.btnAccentUppercase}`}>WATCH VIDEO</a></div>
+              <div className={`${chars.flex} ${chars.largeGap}`}>
+                <a href="google.com" className={`${chars.btn}${chars.btnPrimary}`}>WHITEPAPER </a> 
+                <a href="google.com" className={`${chars.btn}${chars.btnAccentUppercase}`}>WATCH VIDEO</a></div>
             </div>
+          </div>
+          <div className={`${chars.grid} ${chars.featureGrid}`}>
+            
           </div>
         </section>
       </main>
