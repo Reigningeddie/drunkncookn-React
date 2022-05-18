d2d66b5049747fcd68583c978665143d5aa91ed3
Eduardo Aguilar <eaguilar92105@yahoo.com>
2022-05-18 02:30:11 -0700

    fixed synopsis bug

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -7,18 +7,20 @@ import anime from '../images/character-animation.gif'
 
 export default function Characters() {
   return (
-    <container className={`${chars.reset} ${chars.hello}`}>
+    <container className={` ${chars.body}`}>
       <main>
         <section>
-          <div className={chars.grid}>
+        <div className={chars.container}>
+          <div className={`${chars.grid} ${chars.gridColumns}`}>
             <div>
-              <h2 className={`${chars.uppercase} ${chars.textCompressed}`}>WITH XFAI, TRADING WILL <span className= {chars.textPrimary}>NEVER</span> HAVE TO REMAIN CENTRALIZED</h2>
+              <h2 className={`${chars.fs700}${chars.uppercase} ${chars.textCompressed}`}>WITH XFAI, TRADING WILL <span className= {chars.textPrimary}>NEVER</span> HAVE TO REMAIN CENTRALIZED</h2>
               <p>XFai develops tooling by graphing the DeFi space to build <strong>game changing</strong>products to benefit users</p>
               <div className={`${chars.flex} ${chars.largeGap}`}>
                 <a href="google.com" className={`${chars.btn}${chars.btnPrimary}`}>WHITEPAPER </a> 
-                <a href="google.com" className={`${chars.btn}${chars.btnAccent}${chars.uppercase}`}>WATCH VIDEO</a></div>
+                <a href="google.com" className={`${chars.btn}${chars.btnAccent}${chars.uppercase}`}>WATCH VIDEO</a>
+                </div>
+              </div>
             </div>
-          </div>
           <div className={`${chars.grid} ${chars.featureGrid}`}>
             <a href="google.com" aria-label="left-corner"><img src={defaultImg} alt="" /></a>
             <a href="google.com" aria-label="mid-left"><img src={defaultImg} alt="" /></a>
@@ -27,8 +29,9 @@ export default function Characters() {
             <a href="google.com" aria-label="bottom-left"><img src={defaultImg} alt="" /></a>
             <a href="google.com" aria-label="main-character"><img src={defaultImg} alt="" /></a>
             <a href="google.com" aria-label="bottom-right"><img src={anime} alt="" /></a>
+            </div>
           </div>
-        </section>
+          </section>
       </main>
     </container>
   )
