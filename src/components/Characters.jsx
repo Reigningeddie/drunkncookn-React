866f6a485ea49be67904c85062d421fcff551dc4
Eduardo Aguilar <eaguilar92105@yahoo.com>
2022-07-08 22:44:31 -0700

    created characters grid

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -13,7 +13,7 @@ export default function Characters() {
         <div className={chars.container}>
           <div className={`${chars.grid} ${chars.gridColumns}`}>
             <div>
-              <h2 className={`${chars.fs700}${chars.uppercase} ${chars.textCompressed}`}>Characters will be <span className= {chars.textPrimary}>Coming Soon</span></h2>
+              <h2 className={`${chars.fs700}${chars.uppercase} ${chars.text}`}>Character casting will be <span className= {chars.textPrimary}>Coming Soon</span></h2>
               <p>Display will be adjusted as the <strong>cast </strong>list begins to get updated</p>
               <div className={`${chars.flex} ${chars.largeGap}`}>
                 <a href="google.com" className={`${chars.btn}${chars.btnPrimary}`}>Character </a> 
@@ -22,13 +22,13 @@ export default function Characters() {
               </div>
             </div>
           <div className={`${chars.grid} ${chars.featureGrid}`}>
-            <a href="google.com" aria-label="left-corner"><img src={defaultImg} alt="" /></a>
-            <a href="google.com" aria-label="mid-left"><img src={defaultImg} alt="" /></a>
-            <a href="google.com" aria-label="mid-right"><img src={defaultImg} alt="" /></a>
-            <a href="google.com" aria-label="right-corner"><img src={defaultImg} alt="" /></a>
-            <a href="google.com" aria-label="bottom-left"><img src={defaultImg}  alt="" /></a>
-            <a href="google.com" aria-label="main-character"><img src={anime}  alt="" /></a>
-            <a href="google.com" aria-label="bottom-right"><img src={defaultImg}  alt="" /></a>
+            <a href="google.com" aria-label="left-corner bio"><img src={defaultImg} alt="" /></a>
+            <a href="google.com" aria-label="mid-left bio"><img src={defaultImg} alt="" /></a>
+            <a href="google.com" aria-label="mid-right bio"><img src={defaultImg} alt="" /></a>
+            <a href="google.com" aria-label="right-corner bio"><img src={defaultImg} alt="" /></a>
+            <a href="google.com" aria-label="bottom-left bio"><img src={defaultImg}  alt="" /></a>
+            <a href="google.com" aria-label="main-character bio"><img src={anime}  alt="" /></a>
+            <a href="google.com" aria-label="bottom-right bio"><img src={defaultImg}  alt="" /></a>
             </div>
           </div>
           </section>
