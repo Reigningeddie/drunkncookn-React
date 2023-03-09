5dc8e3cd5b457d6247f7021c53450a08dc701f39
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-03-09 00:22:18 -0800

    centered hello comment

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import menu from './menu.module.css'
 
 export default function Menu() {
   return (
-    <>
-    
-    </>
+    <body className={menu.menuBody}>
+      <list className={menu.container}>
+        <p>pic</p>
+        <p>description</p>
+        <p>price</p>
+      </list>
+    </body>
   )
 }
