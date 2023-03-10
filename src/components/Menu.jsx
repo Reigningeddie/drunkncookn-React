9651ee7622bff93dbe1efa8eff27315d179bb58e
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-03-09 17:22:48 -0800

    added a title to menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,11 +4,14 @@ import menu from './menu.module.css'
 export default function Menu() {
   return (
     <body className={menu.menuBody}>
-      <list className={menu.container}>
-        <p>pic</p>
-        <p>description</p>
-        <p>price</p>
-      </list>
+      <container className={menu.container}>
+        <h1 className={menu.title}>Our Menu</h1>
+        <list className={menu.grid}>
+          <p className={menu.item1}>pic</p>
+          <p className={menu.item2}>description</p>
+          <p className={menu.item3}>price</p>
+        </list>
+      </container>
     </body>
   )
 }
