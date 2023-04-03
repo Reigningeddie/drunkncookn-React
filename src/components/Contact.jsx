c6b33e32938e87e23ab1766580e8c1e178a5c561
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-04-03 15:55:24 -0700

    created contact cards

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,28 @@ export default function Contact() {
   return (
     <>
       <body className={cont.contBody}>
-        
+        <container className={cont.container}>
+          <h1 className={cont.title}>Get in touch</h1>
+          <container className={cont.flex}>
+
+            <card className={cont.card}>
+              <p className={cont.pic}>Picture</p>
+              <p className={cont.name}>Eddie</p>
+              <p className={cont.roles}>Roles:</p>
+              <h3 className={cont.social}>Socials</h3>
+              <p className={cont.ig}>Instagram</p>
+            </card>
+
+            <card className={cont.card}>
+              <p className={cont.pic}>Picture</p>
+              <p className={cont.name}>Edward</p>
+              <p className={cont.roles}>Roles:</p>
+              <h3 className={cont.social}>Socials</h3>
+              <p className={cont.ig}>Instagram</p>
+            </card>
+
+          </container>
+        </container>
       </body>
     </>
   )
