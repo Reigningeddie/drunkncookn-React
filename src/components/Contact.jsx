d0c491fe134fa3ccf5fb579b3e39c7667aeab075
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-04-03 14:44:37 -0700

    changed contbody tag to body

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,9 +4,9 @@ import cont from './contact.module.css';
 export default function Contact() {
   return (
     <>
-      <container className={cont.contBody}>
-        hello 
-      </container>
+      <body className={cont.contBody}>
+        
+      </body>
     </>
   )
 }
