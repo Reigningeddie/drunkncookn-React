29ef11f760fca67c259ead61dda6598990e23350
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-04-04 13:23:51 -0700

    remove drop down menu on nav click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,6 @@ export default function Navbar(props) {
 	}
 	// *navbar responsiveness
 	useEffect(() => {
-
 		const changeWidth = () => {
 			setScreenWidth(window.innerWidth);
 		}
@@ -23,8 +22,9 @@ export default function Navbar(props) {
 		return () => {
 			window.removeEventListener('resize', changeWidth)
 		}
-
 	}, [])
+
+
 
 
 	return (
@@ -32,12 +32,12 @@ export default function Navbar(props) {
 			{(toggleMenu || screenWidth > 500) && (
 			<ul className={nav.list}>
 				<Link to="/drunkncookn-React" onClick={() => props.changeAnimate(false)}
-					className={nav.items}>Home</Link>
-				<Link to="/synopsis" onClick={() => props.changeAnimate(true)}
+					className={nav.items} onClickCapture={(toggleNav)}>Home</Link>
+				<Link to="/synopsis" onClick={() => props.changeAnimate(true)} onClickCapture={(toggleNav)}
 					className={nav.items}>Synopsis</Link>
-				<Link to="/characters" onClick={() => props.changeAnimate(true)}
+				<Link to="/characters" onClick={() => props.changeAnimate(true)} onClickCapture={(toggleNav)}
 					className={nav.items}>Characters</Link>
-				<Link to="/menu" onClick={() => props.changeAnimate(true)}
+				<Link to="/menu" onClick={() => props.changeAnimate(true)} onClickCapture={(toggleNav)}
 					className={nav.items}>Menu</Link>
 				{/* <Link to="/media" onClick={() => props.changeAnimate(true)}
 					className={nav.items}>Media</Link>
@@ -45,7 +45,7 @@ export default function Navbar(props) {
 					className={nav.items}>Credits</Link>
 				<Link to="/merch" onClick={() => props.changeAnimate(true)}
 					className={nav.items}>Merch</Link> */}
-				<Link to="/contacts" onClick={() => props.changeAnimate(true)}
+				<Link to="/contacts" onClick={() => props.changeAnimate(true)} onClickCapture={(toggleNav)}
 					className={nav.items}>Contacts</Link>
 			</ul>
 			)}
