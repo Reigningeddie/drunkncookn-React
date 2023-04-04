8f9906f05eeb5736ffea8723cf8b071d3135621b
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-04-04 14:56:16 -0700

    1

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,9 +3,10 @@ import neon from './title.module.css';
 
 export default function Title(props) {
     let animate = props.animateUp ? `${neon.neonTitle} ${neon.animateUp}` : `${neon.neonTitle}`
+    
     return (
         <>
-        <container className={neon.title}>
+        <container className={`${neon.title}`}>
             <h1 className={animate}>Drunkn'Cookn</h1>
         </container>
         </>
