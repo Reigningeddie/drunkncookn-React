ce3467a74772a4b9f2db2db0da3ae1ebc1cba933
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-04-10 20:22:39 -0700

    added style to cards

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
       <body className={cont.contBody}>
         <container className={cont.container}>
           <h1 className={cont.title}>Get in touch</h1>
-          <h2 className={cont.subTitle}>follow Drunkn'cookn on IG:&nbsp;<a href="https://www.instagram.com/drunkncookn/" className={cont.href}>@drunkncookn</a></h2>
+          <h2 className={cont.subTitle}>follow Drunkn'cookn on IG:&nbsp;<a href="https://www.instagram.com/drunkncookn/" className={cont.dTag}>@drunkncookn</a></h2>
           <container className={cont.flex}>
 
             <card className={cont.card}>
@@ -20,7 +20,8 @@ export default function Contact() {
                     <p className={cont.name}>Eduardo</p>
                     <p className={cont.roles}>Roles:</p>
                     <h3 className={cont.social}>Socials</h3>
-                    <p className={cont.ig}>Instagram</p>
+                    <p className={cont.email}>Email: eaguilar92105@yahoo.com</p>
+                    <p className={cont.ig}>Instagram: <a href="https://www.instagram.com/reigningeddie/" className={cont.href}>@reigningeddie</a></p>
                   </section>
                 </container>
             </card>
@@ -35,7 +36,8 @@ export default function Contact() {
                     <p className={cont.name}>Edward</p>
                     <p className={cont.roles}>Roles:</p>
                     <h3 className={cont.social}>Socials</h3>
-                    <p className={cont.ig}>Instagram</p>
+                    <p className={cont.email}>Email: edward.g001@gmail.com</p>
+                    <p className={cont.ig}>Instagram: <a href="https://www.instagram.com/theopticknerv/" className={cont.href}> @theopticknerv</a></p>
                   </section>
                 </container>
             </card>
