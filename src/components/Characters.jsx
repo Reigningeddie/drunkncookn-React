5e1d3c4987764a29e31c2d6124e8246269ad5498
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-04-10 21:51:42 -0700

    added characters

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import chars from './characters.module.css'
-import defaultImg from '../images/default.png'
-import anime from '../images/character-animation.gif'
+import Howard from '../images/Howard.png'
+import Vanessa from '../images/Vanessa.png'
+import Tasha from '../images/Tasha.png'
+import TwoTimes from '../images/Two-Times.png'
+import Hank from '../images/Hank.png'
+import Mark from '../images/Mark.png'
+import Ghost from '../images/Ghost.png'
 
 
 
@@ -23,13 +28,13 @@ export default function Characters() {
             </div>
           <div className={`${chars.grid} ${chars.featureGrid}`}>
                 {/*change p tag to anchor tag to add hyper link */}
-            <p aria-label="left-corner bio"><img src={defaultImg} alt="" className={chars.item}/></p>
-            <p aria-label="mid-left bio"><img src={defaultImg} alt="" className={chars.item}/></p>
-            <p aria-label="mid-right bio"><img src={defaultImg} alt="" className={chars.item}/></p>
-            <p aria-label="right-corner bio"><img src={defaultImg} alt="" className={chars.item}/></p>
-            <p aria-label="bottom-left bio"><img src={defaultImg}  alt="" className={chars.item}/></p>
-            <p aria-label="main-character bio"><img src={anime}  alt="" className={chars.item}/></p>
-            <p aria-label="bottom-right bio"><img src={defaultImg}  alt="" className={chars.item}/></p>
+            <p aria-label="left-corner bio"><img src={Mark} alt="" className={chars.item}/></p>
+            <p aria-label="mid-left bio"><img src={Hank} alt="" className={chars.item}/></p>
+            <p aria-label="mid-right bio"><img src={TwoTimes} alt="" className={chars.item}/></p>
+            <p aria-label="right-corner bio"><img src={Tasha} alt="" className={chars.item}/></p>
+            <p aria-label="bottom-left bio"><img src={Ghost}  alt="" className={chars.item}/></p>
+            <p aria-label="main-character bio"><img src={Howard}  alt="" className={chars.item}/></p>
+            <p aria-label="bottom-right bio"><img src={Vanessa}  alt="" className={chars.item}/></p>
             </div>
           </div>
           </section>
