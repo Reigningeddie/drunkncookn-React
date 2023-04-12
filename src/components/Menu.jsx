ae8c97546160853677a3231aed75e0ba67d0bded
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-04-12 16:50:30 -0700

    reformatted menu html

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,15 +7,15 @@ export default function Menu() {
       <container className={menu.container}>
         <h1 className={menu.title}>Our Menu</h1>
         <container className={menu.menu}>
-
           <button>&larr;</button>
+
           <card className={menu.card}>
             <p className={menu.pic}>Picture</p>
             <p className={menu.des}>Description</p>
             <p className={menu.price}>$15</p>
           </card>
-          <button>&rarr;</button>
 
+          <button>&rarr;</button>
         </container>
       </container>
     </body>
