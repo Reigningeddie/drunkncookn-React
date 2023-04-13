6338b9aef1a4deaae5445d51efe513c5baef3a8c
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-04-12 18:26:19 -0700

    removed import react which is not needed in updated react

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import menu from './menu.module.css'
 import MenuItem from './MenuItems'
 
--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import menu from './menu.module.css'
 
 export default function MenuItems({ item }) {
